9adf2028a23c3e7fa84e8c515a78c5590a918e3f
todd.jollo <todd.jollo@nwea.org>
2018-08-22 10:20:37 -0700

    removing comments from atom settings

--- atom-settings.sh
+++ atom-settings.sh
@@ -6,9 +6,6 @@ ln -s -f $PWD/atom/styles.less ~/.atom/styles.less
 
 ln -s -f $PWD/atom/keymap.cson ~/.atom/keymap.cson
 ln -s -f $PWD/atom/config.cson ~/.atom/config.cson
-# ~/.atom/keymap.cson
-# ~/.atom/config.cson
-# ~/.atom/packages
 
 # use this bash command to regenerate atom-packages.txt
 # apm list --installed --bare > atom-packages.txt
